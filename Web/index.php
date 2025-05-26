ec6124827350ca20a75efe7209c098f76bc07e2e
Alexander Francisco Villacres Guerrero <99373430+afvillacres@users.noreply.github.com>
2025-05-26 09:02:06 -0500

    Add files via upload

---
+++ Web/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Smart Watchs</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <center><h1>Lista de Smart Watch</h1></center>
+    <br>
+    <div class="watch-list">
+        <table border="1" cellpadding="10" cellspacing="0">
+            <thead>
+                <tr>
+                    <th>Modelo</th>
+                    <th>Marca</th>
+                    <th>Precio</th>
+                    <th>Características</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Galaxy Watch 6</td>
+                    <td>Samsung</td>
+                    <td>$250</td>
+                    <td>Pantalla AMOLED, GPS, Resistente al agua</td>
+                </tr>
+                <tr>
+                    <td>Apple Watch Series 9</td>
+                    <td>Apple</td>
+                    <td>$399</td>
+                    <td>ECG, Always-On Display, Fitness Tracking</td>
+                </tr>
+                <tr>
+                    <td>Amazfit GTR 4</td>
+                    <td>Huami</td>
+                    <td>$180</td>
+                    <td>14 días de batería, GPS, Monitor de sueño</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
